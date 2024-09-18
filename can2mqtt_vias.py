7a4ad08811e18720eb8ab687537e1cbc2c6d427d
Dan Berglund <danberglund@outlook.com>
2024-09-18 12:34:25 -0700

    Add current and soc

--- can2mqtt_vias.py
+++ can2mqtt_vias.py
@@ -11,3 +11,13 @@ def divideby10(i):
 def mv2v(i):
     volts = float(i)*.001
     return f"{volts:.3f}"
+
+def floatnegation(i):
+    # We don't want a negative sign on 0
+    current = 0.0 if i == 0.0 else -i
+    return f"{current:.2f}"
+
+def float2decimals(i):
+    #just return the number with 2 decimal places
+    return f"{i:.2f}"
+
